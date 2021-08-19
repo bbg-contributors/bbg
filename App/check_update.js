4d2775ef2d73b6811758ec5578a77282088e2c0f
scientificworld <5648276+scientificworld@user.noreply.gitee.com>
2021-08-19 05:05:21 +0000

    update App/check_update.js. 特喵的没改全。。

--- App/check_update.js
+++ App/check_update.js
@@ -5,7 +5,7 @@ module.exports = function(){
   .then(response => response.json())
   .then(function(data){
     if(currentProgramVersion !== data[data.length - 1]["tag_name"]){
-        window.alert(`当前版本不是最新版本。 \n \n 当前版本：${currentProgramVersion}\n 最新版本：${data[0]["tag_name"]} \n \n 请前往 https://gitee.com/baiyang-lzy/bbg/releases 获取最新更新。`)
+        window.alert(`当前版本不是最新版本。 \n \n 当前版本：${currentProgramVersion}\n 最新版本：${data[data.length - 1]["tag_name"]} \n \n 请前往 https://gitee.com/baiyang-lzy/bbg/releases 获取最新更新。`)
     }else{
         window.alert(`当前已经是最新版本！`)
     }
