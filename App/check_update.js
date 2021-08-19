6e8583602f6f5ffbd363b4d75dc7b539542c0d14
scientificworld <5648276+scientificworld@user.noreply.gitee.com>
2021-08-19 04:43:11 +0000

    update App/check_update.js. 一直删release不是长久之计，虽然我也不知道这么改行不行……你随便加个release我测试一下。

--- App/check_update.js
+++ App/check_update.js
@@ -1,10 +1,10 @@
 const currentProgramVersion = require("./currentProgramVersion.js").toString();
 
 module.exports = function(){
-    fetch('https://gitee.com/api/v5/repos/baiyang-lzy/bbg/releases?page=1&per_page=20')
+    fetch('https://gitee.com/api/v5/repos/baiyang-lzy/bbg/releases')
   .then(response => response.json())
   .then(function(data){
-    if(currentProgramVersion !== data[0]["tag_name"]){
+    if(currentProgramVersion !== data[data.length - 1]["tag_name"]){
         window.alert(`当前版本不是最新版本。 \n \n 当前版本：${currentProgramVersion}\n 最新版本：${data[0]["tag_name"]} \n \n 请前往 https://gitee.com/baiyang-lzy/bbg/releases 获取最新更新。`)
     }else{
         window.alert(`当前已经是最新版本！`)
