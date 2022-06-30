4dee8fb6b57497811423d51d9ab6b0952adf553b
chihuo2104 <chihuo2104@moekonnyaku.com>
2022-06-30 22:38:39 +0800

    chore: improve code style

--- App/apply_thirdparty_theme_v2.js
+++ App/apply_thirdparty_theme_v2.js
@@ -13,19 +13,19 @@ module.exports = function () {
     window.alert("你没有选择任何主题文件。博客的主题将不会改变。");
     window.location.reload();
   } else {
-    let zip = new admZip(third_party_theme_path[0])
-    zip.extractAllTo(rootDir, true)
-    let content = zip.getEntries()
-    let wcontent = []
+    let zip = new admZip(third_party_theme_path[0]);
+    zip.extractAllTo(rootDir, true);
+    let content = zip.getEntries();
+    let wcontent = [];
     content.forEach((entry) => {
-      wcontent.push(entry.entryName)
+      wcontent.push(entry.entryName);
     })
-    blog["全局主题设置"]["是否使用第三方主题"] = true
-    blog["全局主题设置"]["若使用第三方主题，是否来自本地文件"] = true
-    blog["全局主题设置"]["若使用来自主题商店的第三方主题，则主题名为"] = ""
-    blog["全局主题设置"]["若使用来自主题商店的第三方主题，则主题的更新发布日期为"] = ""
+    blog["全局主题设置"]["是否使用第三方主题"] = true;
+    blog["全局主题设置"]["若使用第三方主题，是否来自本地文件"] = true;
+    blog["全局主题设置"]["若使用来自主题商店的第三方主题，则主题名为"] = "";
+    blog["全局主题设置"]["若使用来自主题商店的第三方主题，则主题的更新发布日期为"] = "";
     blog["全局主题设置"]["第三方主题版本"] = "v2";
-    blog["全局主题设置"]["第三方主题文件内容"] = wcontent
+    blog["全局主题设置"]["第三方主题文件内容"] = wcontent;
     BlogInstance.writeBlogData();
     window.alert("已经更换为第三方主题。");
     window.location.reload();
