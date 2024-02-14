f443e39f15b2999739ac5ce4c3047a66222324a0
Lockinwize Lolite <mzwing@mzwing.eu.org>
2024-02-15 07:51:24 +0800

    feat: build native deb (#286)
    * feat: build native deb
    
    * fix: version error
    
    * debug(deb_build_files/ci_prepare.js): no output
    
    * fix(build deb ): version typo
    
    * fix: path typo(pack arm64 deb)
    
    * fix: rm permission

--- deb_build_files/ci_prepare.js
+++ deb_build_files/ci_prepare.js
@@ -8,26 +8,12 @@ This script is designed to be run in CI environment. Please do not run it yourse
 */
 
 
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync } = require("fs");
 
-const AppJson = readFileSync(`${__dirname}/../App/App.json`, {encoding: "utf-8"});
+const AppJson = readFileSync(`${__dirname}/../App/App.json`, { encoding: "utf-8" });
 
 const Content = JSON.parse(AppJson);
 
 const Version = Content["VersionNumber"];
 
-let sources = new Object();
-
-sources["amd64"] = readFileSync(`${__dirname}/amd64/control`, {encoding: "utf-8"});
-
-sources["arm64"] = readFileSync(`${__dirname}/arm64/control`, {encoding: "utf-8"});
-
-// fill in version number
-
-sources["amd64"] = sources["amd64"].replaceAll("modify_this_value_to_actual_bbg_version", Version);
-sources["arm64"] = sources["arm64"].replaceAll("modify_this_value_to_actual_bbg_version", Version);
-
-// save
-
-writeFileSync(`${__dirname}/amd64/control`, sources["amd64"]);
-writeFileSync(`${__dirname}/arm64/control`, sources["arm64"]);
+console.log(Version);
